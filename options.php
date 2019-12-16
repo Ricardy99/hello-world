75ceb1b1a207b51084e3f6b8ebd9fb8ce5981b75
Ricardy99 <ricardyc@gmail.com>
2019-12-16 02:13:39 -0500

    updates options.php
    final updates on options file, mainly removed lines there for testing

--- options.php
+++ options.php
@@ -7,19 +7,12 @@
 <body>
 
 <h1 align='center'> Options </h1>
-<p1><b>Bet you're disappointed lol</b></p1>
 
 <br>
-<p2>Don't worry, I'll eventually add stuff but for now, well...</p2>
-<p3>You can go back to the <a href="index.php">home page</a></p3>
+<p1>You can go back to the <a href="index.php">home page</a></p1>
 
-<p3>You can also check your <a href="usr_agent.php">user agent</a> if you want to</p3>
+<p2>You can also check your <a href="usr_agent.php">user agent</a> if you want to</p2>
 <p3>or check the <a href="get_coninfo.php">contact</a> table in the info database</p3>
-
-<div class = 'cont'>
-	<h3>This is a heading heading 3</h3>
-	<p>This is a paragraph. just 'p'</p>
-  </div>
 
 </body>
 </html>
