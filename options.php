8d23a1ad1815e1b5491e0f143325011dc041a91c
Ricardy99 <ricardyc@gmail.com>
2019-12-16 02:24:57 -0500

    added an image gallery
    or something that looks like it, took it from w3schools, will try to personalize it later

--- options.php
+++ options.php
@@ -14,5 +14,34 @@
 <p2>You can also check your <a href="usr_agent.php">user agent</a> if you want to</p2>
 <p3>or check the <a href="get_coninfo.php">contact</a> table in the info database</p3>
 
+<div class="gallery">
+  <a target="_blank" href="images/img_5terre.jpg">
+    <img src="images/img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="images/img_forest.jpg">
+    <img src="images/img_forest.jpg" alt="Forest" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="images/img_lights.jpg">
+    <img src="images/img_lights.jpg" alt="Northern Lights" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="images/img_mountains.jpg">
+    <img src="images/img_mountains.jpg" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+
 </body>
 </html>
